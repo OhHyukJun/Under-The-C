d2fd4976606c1a058a0c635cb719195dec670dad
Osongmin <autumninmoon@gmail.com>
2023-07-14 21:51:20 +0900

    feat: main page using tailwind

--- frontend-react/src/Pages/Main/Lecture.tsx
+++ frontend-react/src/Pages/Main/Lecture.tsx
@@ -2,11 +2,20 @@ import { ILecture } from '../../Atoms/Lecture';
 
 export const Lecture = (props: ILecture) => {
   return (
-	<>
-	<div >
-		<div>Lecture</div>
-		{props.title}
+	<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-6">
+		<div className="md:flex">
+			<div className="p-8">
+				<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+					{props.lecture}
+				</div>
+					<a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+						{props.title}
+					</a>
+					<p className="mt-2 text-slate-500">
+						{props.content}
+					</p>
+			</div>
+		</div>
 	</div>
-	</>
   );
 };
--- frontend-react/src/Pages/Main/Main.tsx
+++ frontend-react/src/Pages/Main/Main.tsx
@@ -7,27 +7,26 @@ import { fetchPost } from '../../Api/api';
 
 const Main = () => {
 	const [ lectureList, setLectureList ] = useRecoilState(lectureListState);
-  const { isLoading, isError, data } = useQuery(['lecture'], fetchPost, {
-		onSuccess: (data) => setLectureList(data)
-		//기본 캐시 타임 == 5min
-	});
+	const { isLoading, isError, data } = useQuery(['lecture'], fetchPost, {
+			onSuccess: (data) => setLectureList(data)
+			//기본 캐시 타임 == 5min
+		});
 
-  if (isLoading)
-    return <h2>Loading...</h2>
-	
-	if (isError)
-		return <h2>Error...</h2>
+	if (isLoading)
+		return <h2>Loading...</h2>
+		
+		if (isError)
+			return <h2>Error...</h2>
 
-  return (
-    <>
-      <Header />
-      <h1>Main page</h1>
-      {
-		lectureList?.map((item: ILecture) => {
-			return <Lecture key={item.id} {...item} /> })
-	  }
-    </>
-  );
+	return (
+			<div className="sm p-6 mx-auto bg-neutral-100 ">
+				<Header />
+				{
+					lectureList?.map((item: ILecture) => {
+						return <Lecture key={item.id} {...item} /> })
+				}
+			</div>
+  	);
 };
 
 export default Main;

7cef32c4a22700da24f43621bd2ab4d333ae90e4
OhHyukJun <kmh7277@naver.com>
2023-07-14 11:40:22 +0900

    Feat: 라우터 경로 추가

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -4,7 +4,10 @@ import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'reco
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Board from './Pages/Board/Board';
+import LoginPage from './Pages/Auth/LoginPage';
 import RegisterPage from './Pages/Auth/RegisterPage';
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
 import Main from './Pages/Main/Main';
 import UserPage from './Pages/User/UserPage';
 
@@ -13,6 +16,7 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <>
+    <Header/>
 	<RecoilRoot>
 		<QueryClientProvider client={queryClient} >
 			<BrowserRouter>
@@ -20,12 +24,14 @@ const App = () => {
 				<Route path="/" element={<Main/>}/>
 				<Route path="/board" element={<Board/>}/>
 				<Route path="/UserPage" element={<UserPage/>}/>
+				<Route path='/LoginPage' element={<LoginPage/>}/>
 				<Route path="/RegisterPage" element={<RegisterPage/>}/>
 				</Routes>
 			</BrowserRouter>
 		</QueryClientProvider>
 	</RecoilRoot>
-	</>
+      <Footer/>
+    </>
   )
 }
 

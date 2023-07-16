5cb43e03ad27a85fd46ae653678d9a5f03befd2e
root <root@JiGang>
2023-07-17 03:31:37 +0900

    Feat: 예외처리 및 강의 평가 추천 삭제 기능 추가

--- backend-spring/src/main/java/UnderTheC/DeepSea/controller/RecommendationController.java
+++ backend-spring/src/main/java/UnderTheC/DeepSea/controller/RecommendationController.java
@@ -30,13 +30,14 @@ public class RecommendationController {
     }
 
     @PostMapping("/recommend")
-    @Operation(summary = "강의 평가 추천", description = "user id, evaluationID 받아와서 recommendation 테이블에 추가", responses = {
+    @Operation(summary = "강의 평가 추천, 취소", description = "user id, evaluationID 받아와서 recommendation 테이블에 추가", responses = {
             @ApiResponse(responseCode = "200", description = "성공")
     })
     public Recommendation recommendEvaluation(@RequestBody Recommendation recommendationRequest
     ) {
         String userID = recommendationRequest.getUserID();
         int evaluationID = recommendationRequest.getEvaluationID();
+
         Recommendation recommendation = null;
         recommendation = new Recommendation();
         recommendation.setUserID(userID);
@@ -44,22 +45,39 @@ public class RecommendationController {
         recommendation.setCreated(recommendationRequest.getCreated());
 
 
-        Optional<Evaluation> evaluation = null;
-        evaluation = evaluationRepository.findById(String.valueOf(evaluationID));
-        int temp =evaluation.get().getLikeCount();
+        //같은 userID와 EvaluationID를 가진 Recommendation 객체가 있다면, 추천 삭제
+        Optional<Recommendation> existingRecommendation = recommendationRepository.findByUserIDAndEvaluationID(userID, evaluationID);
+        Optional<Evaluation> evaluation = evaluationRepository.findById(String.valueOf(evaluationID));
+        if (evaluation.isPresent()) {
+            if (existingRecommendation.isPresent()) {
+                recommendationRepository.delete(existingRecommendation.get());
 
-        if(userID.equals(evaluation.get().getUserID()) && String.valueOf(evaluationID).equals(evaluation.get().getEvaluationID())){
-            temp--; //추천 삭제
-            evaluation.get().setLikeCount(temp);
-            evaluationRepository.save(evaluation.get());
-        }else{
-            temp++; //추천
-            evaluation.get().setLikeCount(temp);
-            evaluationRepository.save(evaluation.get());
+                if (evaluation.isPresent()) {
+                    int temp = evaluation.get().getLikeCount();
+                    temp--; // 추천 취소
+                    evaluation.get().setLikeCount(temp);
+                    evaluationRepository.save(evaluation.get());
+                }
+            } else {
+                if (evaluation.isPresent()) {
+                    int temp = evaluation.get().getLikeCount();
+                    temp++; //추천
+                    evaluation.get().setLikeCount(temp);
+                    evaluationRepository.save(evaluation.get());
+                    return recommendationRepository.save(recommendation);
+                }
+            }
+            return recommendationRequest;
+        } else {
+            throw new IllegalArgumentException("평가 ID에 해당하는 객체를 찾을 수 없습니다.");
         }
-        return recommendation;
     }
-
-
-
+    @GetMapping("/all")
+    @Operation(summary = "모든 추천 조회", description = "recommendation 테이블에 있는 모든 추천 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "성공")
+    })
+    public List<Recommendation> getAllRecommendations() {
+        return recommendationRepository.findAll();
+    }
 }
+
--- backend-spring/src/main/java/UnderTheC/DeepSea/repository/EvaluationRepository.java
+++ backend-spring/src/main/java/UnderTheC/DeepSea/repository/EvaluationRepository.java
@@ -6,6 +6,7 @@ import org.springdoc.core.converters.models.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.Date;
 @Repository
@@ -13,4 +14,12 @@ public interface EvaluationRepository extends JpaRepository<Evaluation, String> 
     List<Evaluation> findAllByLectureNameOrderByLikeCountAsc(String lectureName);
     List<Evaluation> findAllByLectureNameOrderByCreatedDesc(String lectureName);
     List<Evaluation> findByLectureName(String lectureName);
+
+    List<Evaluation> findAllByLectureNameAndLectureDivideOrderByCreatedDesc(String lectureName, String lectureDivide);
+
+    List<Evaluation> findAllByLectureNameAndLectureDivideOrderByLikeCountDesc(String lectureName, String lectureDivide);
+
+    List<Evaluation> findAllByLectureNameOrderByLikeCountDesc(String lectureName);
+
+    Optional<Evaluation> findByUserIDAndLectureName(String userID, String lectureName);
 }

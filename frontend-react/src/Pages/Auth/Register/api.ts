a047c87dcaf00401f5bd61fb780d8245518e7726
OhHyukJun <kmh7277@naver.com>
2023-07-16 19:50:22 +0900

    Style: uri 주소 변경

--- frontend-react/src/Pages/Auth/Register/api.ts
+++ frontend-react/src/Pages/Auth/Register/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 //import Member from './Member';
 
+//const URI = "http://localhost:4000/members";
 const URI = "http://115.85.181.92:8090/user";
 
 export const memberPost = async (data: any) => {

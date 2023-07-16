d46801bb634bf90f7c4d88f6aa402bc221d283f2
OhHyukJun <kmh7277@naver.com>
2023-07-16 19:39:42 +0900

    Feat: api 코드 추가

--- frontend-react/src/Pages/Auth/RegisterPage.tsx
+++ frontend-react/src/Pages/Auth/RegisterPage.tsx
@@ -1,17 +1,28 @@
+import { useMutation } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { memberPost } from './Register/api';
+import { Member } from './Register/Member';
 
-
-interface Member {
+/*interface Member {
+    id: string;
 	  email: string;
     password: string;
 }
-
+*/
 const RegisterPage = () => {
   const { register, handleSubmit, formState: { errors } , reset} = useForm<Member>();
-
+  const mutation = useMutation(memberPost, {
+    onSuccess: (data) => {
+      console.log('Response:', data);
+    },
+    onError: (error) => {
+      console.error('Error:', error);
+    },
+  });
 
   const onSubmit = (data: Member) => {
+    mutation.mutate(data);
     console.log('Member:', data);
     reset();
   };
@@ -30,6 +41,14 @@ const RegisterPage = () => {
                   회원가입
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                  <div>
+                      <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">아이디</label>
+                      <input 
+                      type="id"
+                      {...register('id',{required: '아이디을 입력하세요'})}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                  </div>
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
                       <input 

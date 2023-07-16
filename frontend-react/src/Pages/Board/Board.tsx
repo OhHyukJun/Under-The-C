ec035d1fd0f3202874969ef01f1213e487f8d612
OhHyukJun <kmh7277@naver.com>
2023-07-16 22:42:53 +0900

    Feat: 글쓰기 작성 페이지 구현

--- frontend-react/src/Pages/Board/Board.tsx
+++ frontend-react/src/Pages/Board/Board.tsx
@@ -1,8 +1,99 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { ILecture } from '../../Atoms/Lecture';
+import { lectorePost } from './api';
+
 const Board = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<ILecture>();
+  const navigate = useNavigate();
+  const mutation = useMutation(lectorePost, {
+    onSuccess: (data) => {
+      navigate("/");
+      console.log('Response:', data);
+    },
+    onError: (error) => {
+      console.error('Error:', error);
+    },
+  });
+  const onSubmit = (data: ILecture) => {
+    mutation.mutate(data);
+    console.log('ILecture:', data);
+    reset();
+  };
+
   return (
-      <>
-          <h1>글쓰기 페이지</h1>
-      </>
+    <section className="bg-gray-100 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">
+                제목
+              </label>
+              <input
+                type="text"
+                id="title"
+                {...register('title', { required: '제목을 입력하세요' })}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
+              />
+            </div>
+            <div>
+              <label htmlFor="rating" className="block mb-2 text-sm font-medium text-gray-900">
+                별점
+              </label>
+              <input
+                type="number"
+                id="rating"
+                placeholder="1~5 입력"
+                {...register('rating', {min:1 , max:5 })}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
+              />
+            </div>
+            <div>
+              <label htmlFor="lecture" className="block mb-2 text-sm font-medium text-gray-900">
+                강의명
+              </label>
+              <input
+                type="text"
+                id="lecture"
+                {...register('lecture', { required: '강의명을 입력하세요' })}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
+              />
+            </div>
+            <div>
+              <label htmlFor="processor" className="block mb-2 text-sm font-medium text-gray-900">
+                교수명
+              </label>
+              <input
+                type="text"
+                id="professor"
+                {...register("professor", { required: '강의명을 입력하세요' })}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
+              />
+            </div>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="content" className="block mb-2 text-sm font-medium text-gray-900">
+              강의 설명
+            </label>
+            <textarea
+              {...register('content', { minLength:{
+                value: 6,
+                message: '6글자 이상 입력해주세요',
+              },  
+            })}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 h-48 resize-none"
+            ></textarea>
+          </div>
+          <button type="submit" className="w-full text-gray-900 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+            제출하기
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 

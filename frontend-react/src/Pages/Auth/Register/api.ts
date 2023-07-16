f17e504a8969e19b0a7cfdb6105c52ea60e3dfc8
OhHyukJun <kmh7277@naver.com>
2023-07-16 19:39:56 +0900

    Rename: 경로 파일 생성

---
+++ frontend-react/src/Pages/Auth/Register/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+//import Member from './Member';
+
+const URI = "http://115.85.181.92:8090/user";
+
+export const memberPost = async (data: any) => {
+    const res = await axios.post(URI, data);
+    return res.data;
+  };

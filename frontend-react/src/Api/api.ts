cbffc50a4ac24f0dd10d0d4a1a62e42bda3ac6f8
Osongmin <autumninmoon@gmail.com>
2023-07-17 22:39:22 +0900

    fix: rest api cors

--- frontend-react/src/Api/api.ts
+++ frontend-react/src/Api/api.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const URI = "http://115.85.181.92:8090";
-axios.defaults.withCredentials = true;
-
 export const fetchPost = async () => {
-	const res = await axios.get(URI + '/evaluation/find');
+	//console.log('/evaluation/find')
+	//console.log(process.env)
+	const res = await axios.get('http://localhost:8090/evaluation/find');
 	console.log(res.data);
 	return res.data;
 }
 
 export const fetchLogin = async (id: string, password: string) => {
-	const res = await axios.post(URI + '/login', {
+	const res = await axios.post('/login', {
+	//const res = await axios.post(URI + '/login', {
 		id: id,
 		password: password,
 	});

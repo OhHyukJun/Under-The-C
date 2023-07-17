e7c05134233ffd76cb39f52646751c51aa209457
OhHyukJun <kmh7277@naver.com>
2023-07-17 17:44:31 +0900

    Style: api.ts 파일 경로 수정

--- frontend-react/src/Pages/Auth/Register/api.ts
+++ frontend-react/src/Pages/Auth/Register/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-//import Member from './Member';
 
-//const URI = "http://localhost:4000/members";
-const URI = `${process.env.SPRING_HOST}:${process.env.SPRING_PORT}/user/add`;
+const URI = `http://${process.env.REACT_APP_SPRING_HOST}:${process.env.REACT_APP_SPRING_PORT}/user/add`;
 
 export const memberPost = async (data: any) => {
-    const res = await axios.post(URI, data);
-    return res.data;
-  };
+  console.log("URI:", URI);
+  const res = await axios.post(URI, data);
+  return res.data;
+};
--- frontend-react/src/Pages/Auth/RegisterPage.tsx
+++ frontend-react/src/Pages/Auth/RegisterPage.tsx
@@ -49,7 +49,7 @@ const RegisterPage = () => {
                       <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">아이디</label>
                       <input 
                       type="id"
-                      {...register('id',{required: '아이디을 입력하세요'})}
+                      {...register("id",{required: '아이디을 입력하세요'})}
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
@@ -57,27 +57,14 @@ const RegisterPage = () => {
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
                       <input 
                       type="email"
-                      {...register('email',{required: '이메일을 입력하세요'})}
+                      {...register("email",{required: '이메일을 입력하세요'})}
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
                   <div>
                       <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호</label>
                       <input  type="password"
-                      {...register('password', { 
-                          required: '비밀번호를 입력', 
-                              minLength:{
-                                  value: 6,
-                                  message: '비밀번호는 최소 6자리입니다.',
-                              },
-                          })}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        />
-                  </div>
-                  <div>
-                      <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호 확인</label>
-                      <input  type="password"
-                      {...register('password', { 
+                      {...register("password", { 
                           required: '비밀번호를 입력', 
                               minLength:{
                                   value: 6,

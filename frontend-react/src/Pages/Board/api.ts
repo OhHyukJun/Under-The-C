5b9a973444e79e26bf254e5b9170905827b4e958
OhHyukJun <kmh7277@naver.com>
2023-07-17 15:52:44 +0900

    Feat:평가 등록 api 경로 변경

--- frontend-react/src/Pages/Board/api.ts
+++ frontend-react/src/Pages/Board/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const URI = "http://localhost:4000/posts";
-const URI = "http://115.85.181.92:8090//lecture";
+const URI = "http://115.85.181.92:8090/evaluation/add";
 
 export const lectorePost = async (data: any) => {
     data.rating = parseInt(data.rating);

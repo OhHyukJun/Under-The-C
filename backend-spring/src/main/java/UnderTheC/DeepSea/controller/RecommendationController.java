3b0a8da3eda1d740b9fda5253610ce6acc52a307
root <root@JiGang>
2023-07-17 19:52:21 +0900

    Fix: 로그인 세션 인증버그 수정

--- backend-spring/src/main/java/UnderTheC/DeepSea/controller/RecommendationController.java
+++ backend-spring/src/main/java/UnderTheC/DeepSea/controller/RecommendationController.java
@@ -1,11 +1,13 @@
 package UnderTheC.DeepSea.controller;
 import UnderTheC.DeepSea.Entity.Evaluation;
 import UnderTheC.DeepSea.Entity.Recommendation;
+import UnderTheC.DeepSea.Entity.User;
 import UnderTheC.DeepSea.repository.RecommendationRepository;
 import UnderTheC.DeepSea.repository.EvaluationRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -31,30 +33,35 @@ public class RecommendationController {
     }
 
     @PostMapping("/recommend")
-    @Operation(summary = "강의 평가 추천, 취소", description = "user id, evaluationID 받아와서 recommendation 테이블에 추가", responses = {
+    @Operation(summary = "강의 평가 추천, 취소", description = "user id, evaluationId 받아와서 recommendation 테이블에 추가", responses = {
             @ApiResponse(responseCode = "200", description = "성공")
     })
-    public Recommendation recommendEvaluation(@RequestBody Recommendation recommendationRequest, HttpSession session
+    public Recommendation recommendEvaluation(@RequestBody Recommendation recommendationRequest, HttpServletRequest request
     ) {
-        String userID = (String) session.getAttribute("userID"); // 세션에서 로그인한 사용자의 userID 가져오기
-        // 세션에 로그인 정보가 없는 경우 또는 userID가 null인 경우 예외 처리
-        if (userID == null) {
+        HttpSession session = request.getSession();
+
+        // 세션에 로그인 정보가 없는 경우 또는 userId가 null인 경우 예외 처리
+        if (session == null) {
             throw new IllegalArgumentException("로그인이 필요합니다.");
         }
-        int evaluationID = recommendationRequest.getEvaluationID();
+        User user = new User();
+        user = (User)request.getSession(false).getAttribute("loginUser");// 세션에서 로그인한 사용자의 userId 가져오기
+
+        String userId = user.getId();
+
+        int evaluationId = recommendationRequest.getEvaluationId();
 
         Recommendation recommendation = new Recommendation();
-        recommendation.setUserID(userID);
-        recommendation.setEvaluationID(evaluationID);
+        recommendation.setUserId(userId);
+        recommendation.setEvaluationId(evaluationId);
         recommendation.setCreated(recommendationRequest.getCreated());
 
 
-        //같은 userID와 EvaluationID를 가진 Recommendation 객체가 있다면, 추천 삭제
-        Optional<Recommendation> existingRecommendation = recommendationRepository.findByUserIDAndEvaluationID(userID, evaluationID);
-        Optional<Evaluation> evaluation = evaluationRepository.findById(String.valueOf(evaluationID));
+        //같은 userId와 EvaluationId를 가진 Recommendation 객체가 있다면, 추천 삭제
+        Optional<Recommendation> existingRecommendation = recommendationRepository.findByUserIdAndEvaluationId(userId, evaluationId);
+        Optional<Evaluation> evaluation = evaluationRepository.findById(String.valueOf(evaluationId));
         if (evaluation.isPresent()) {
             if (existingRecommendation.isPresent()) {
-                    recommendationRepository.delete(existingRecommendation.get());
                     recommendationRepository.delete(existingRecommendation.get());
                     int temp = evaluation.get().getLikeCount();
                     temp--; // 추천 취소
@@ -69,7 +76,7 @@ public class RecommendationController {
             }
             return recommendationRequest;
         } else {
-            throw new IllegalArgumentException("평가 ID에 해당하는 객체를 찾을 수 없습니다.");
+            throw new IllegalArgumentException("평가 Id에 해당하는 객체를 찾을 수 없습니다.");
         }
     }
     @GetMapping("/all")

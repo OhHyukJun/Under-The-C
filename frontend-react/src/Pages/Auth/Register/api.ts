3b7d883322bc49fe1490627b6169b9c638c32d34
OhHyukJun <kmh7277@naver.com>
2023-07-17 17:00:58 +0900

    Feat: API 경로 수정

--- frontend-react/src/Pages/Auth/Register/api.ts
+++ frontend-react/src/Pages/Auth/Register/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //import Member from './Member';
 
 //const URI = "http://localhost:4000/members";
-const URI = "${SPRING_HOST}:${SPRING_PORT}/user/add";
+const URI = `${process.env.SPRING_HOST}:${process.env.SPRING_PORT}/user/add`;
 
 export const memberPost = async (data: any) => {
     const res = await axios.post(URI, data);
